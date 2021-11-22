fa0db5551d7272b09c9f4c563420d2c2ec3a869b
Равшан <tolibjonovrvashan@gmail.com>
2021-11-22 22:01:41 +0500

    first api added

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,15 @@
     <TodoList  :todos="todos" @delete-todo="deleteTodo"/>
     <input v-model.lazy="messages" placeholder="edit me" ref="input" v-on:keyup.enter="addTodos"/>
     <button class="btn" @click="addTodos">Add</button>
+    <div class="api">
+      <a  href= "https://www.pexels.com"> Фотографии предоставлены Pexels </a>
+      <a  href= "https://www.pexels.com"> <IMG SRC = "https://images.pexels.com/lib/api/pexels-white.png" /> </a>
+      <a  href= "https://www.pexels.com"> <IMG SRC = "https://images.pexels.com/lib/api/pexels.png" /> </a>
+      <div>This <a href="https://www.pexels.com/photo/food-dinner-lunch-meal-4147875">Photo</a> was taken by <a href="https://www.pexels.com/@daria">Daria</a> on Pexels.
+</div>
+ 
+
+    </div>
   </div>
 </template>
 
@@ -35,6 +44,19 @@ export default{
 }
 
 </script>
+<style>
+  body {
+    background: rgba(0,0,0,0.7);
+  }
+  .api {
+    margin: 20px 0;
+    display: flex;
+    flex-direction: column;
+  }
+  .api a {
+    margin: 10px 0;
+  }
+</style>
 <style scoped>
   #app {
     display: block;
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createClient } from 'pexels';
+
+const client = createClient('563492ad6f9170000100000101ce71bbc39b4a5ab970ff7eb4ded018');
 
 
 createApp(App).mount('#app')
